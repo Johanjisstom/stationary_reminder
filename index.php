871f0b78607ace291cb8695f776169baca643dd1
Johanjisstom <johan.tom@student.otahuhucollege.school.nz>
2026-08-21 10:35:01 +1200

    double Hello

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <h1>Stationary Reminder</h1>
 <p>Welcome to the Stationary Reminder website, where you put your daily tasks, reminding your stationary etc. Come sign up to start or sign in to see your progress</P>
 <h1> HEllo World!</h1>
+
+<h1> HEllo World!</h1>
+
