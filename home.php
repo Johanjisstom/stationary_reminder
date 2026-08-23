f27909b3bebd9cb008d9061399b1397789a88975
Johanjisstom <johan.tom@student.otahuhucollege.school.nz>
2026-08-24 09:11:46 +1200

    home.php would work

---
+++ home.php
@@ -0,0 +1,2 @@
+<h1>Stationary Reminder</h1>
+<p>Welcome to the Stationary Reminder website, where you put your daily tasks, reminding your stationary etc. Come sign up to start or sign in to see your progress</p>
